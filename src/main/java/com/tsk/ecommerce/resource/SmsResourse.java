6f6013af16dfdeae6687db9db4d2173ee4c19a7b
maminiaina marco <maminiainamarco@gmail.com>
2021-11-20 20:36:51 +0300

    configuration twilio

---
+++ src/main/java/com/tsk/ecommerce/resource/SmsResourse.java
@@ -0,0 +1,31 @@
+package com.tsk.ecommerce.resource;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tsk.ecommerce.entities.Address;
+import com.tsk.ecommerce.model.SmsRequest;
+import com.tsk.ecommerce.utils.sms.SmsSender;
+
+@RestController
+@RequestMapping("api/v1")
+public class SmsResourse {
+
+	@Autowired
+	private SmsSender smsSender;
+	
+	@PostMapping("/sms")
+	public void sendSms(@Valid @RequestBody SmsRequest smsRequest) {
+		
+		smsSender.sendSms(smsRequest);
+		
+	}
+	
+}
